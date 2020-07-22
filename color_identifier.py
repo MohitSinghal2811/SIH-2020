756e0c7f57219ab89c6854dfc4dba39225f83765
MOHIT <2018eeb1165@iitrpr.ac.in>
2020-07-22 13:27:36 +0530

    Pushing some test images

--- color_identifier.py
+++ color_identifier.py
@@ -2,14 +2,14 @@ import numpy as np
 from cv2 import cv2
 
 
-def color_segmenter(frame):
+def color_segmenter(frame, img_name):
 
     frame = np.array(frame)
     hsv_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
 
 
-    low_colors = [[0, 0, 193.8], [0, 0, 0], [94,80,2], [30,85,85.05], [170, 100, 0],[0,0,66.3]]
-    high_colors = [[131.00, 30.00, 255], [255, 255, 25], [126, 255, 255], [102.0, 255, 255], [180, 255, 255],[255,33,230.00]]
+    low_colors = [[0, 0, 193], [0, 0, 0], [94,80,2], [30,85,85], [0, 150, 50],[0,0,66]]
+    high_colors = [[131, 30, 255], [255, 255, 25], [126, 255, 255], [10, 255, 255], [180, 255, 255],[255,33,230]]
     color_names = ["white", "black", "blue", "green", "red","grey"]
 
     ans = ("UNK", 0)
@@ -28,13 +28,14 @@ def color_segmenter(frame):
             continue
         elif(ad[1][1] > ans[1]):
             ans = (color_names[x], ad[1][1])
-        cv2.imwrite(path + str(color_names[x]) + str(".jpg"), color_mask)
+        cv2.imwrite(path + str(img_name) + "_" + str(color_names[x]) + str(".jpg"), color_mask)
 
     return(ans[0])
 
 
 
 if __name__ == "__main__":
-    path = "alpr-unconstrained/samples/only_cars/10.png"
+    path = "alpr-unconstrained/samples/Indian_vehicles/4.png"
     frame = cv2.imread(path)
-    print(color_segmenter(frame))      
+    arr = path.split("/")
+    print(color_segmenter(frame, arr[-1][:-4]))      
